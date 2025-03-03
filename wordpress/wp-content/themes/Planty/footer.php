4611e22852e49aca0cea1202f1a0df2f7440da62
Enola Lamy <enola.vigneron54@gmail.com>
2025-03-03 22:50:12 +0100

    Ajout cannettes

--- wordpress/wp-content/themes/Planty/footer.php
+++ wordpress/wp-content/themes/Planty/footer.php
@@ -7,6 +7,9 @@
  */
 
 ?>
+<div class="cannette-section">
+    <img src="http://localhost/planty/wordpress/wp-content/uploads/2025/03/Mask-group-3.png" alt="Collection de cannettes" class="cannette-image">
+</div>
 </main><!-- #main -->
 
 <?php do_action('ocean_after_main'); ?>
